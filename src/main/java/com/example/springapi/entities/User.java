14d32efb0b61a14793f9f99cb7e05934a042694c
DESKTOP-3Q8IA8H\execute-Jhony <jhonymarlon@gmail.com>
2022-03-31 16:26:16 -0300

    Configurando banco de dados

--- src/main/java/com/example/springapi/entities/User.java
+++ src/main/java/com/example/springapi/entities/User.java
@@ -1,9 +1,16 @@
 package com.example.springapi.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
