f543814c1b0d6bd8191545b56cb18c878b206616
DESKTOP-R8GAO0T\JhonyPC <jhonymarlon@gmail.com>
2022-04-05 21:40:53 -0300

    Fixing bug of get /products

--- src/main/java/com/example/springapi/resources/CategoryResource.java
+++ src/main/java/com/example/springapi/resources/CategoryResource.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
+
     @Autowired
     private CategoryService service;
 
--- src/main/java/com/example/springapi/services/ProductService.java
+++ src/main/java/com/example/springapi/services/ProductService.java
@@ -3,10 +3,12 @@ package com.example.springapi.services;
 import com.example.springapi.entities.Product;
 import com.example.springapi.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ProductService {
     @Autowired
     private ProductRepository repository;
