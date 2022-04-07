9d265716693625b8d219111d76fa885b0ce7e898
DESKTOP-3Q8IA8H\execute-Jhony <jhonymarlon@gmail.com>
2022-04-07 17:52:15 -0300

    Configurando Payment

--- src/main/java/com/example/springapi/config/TestConfig.java
+++ src/main/java/com/example/springapi/config/TestConfig.java
@@ -69,5 +69,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll((Arrays.asList(oi1,oi2,oi3,oi4)));
+
+        Payment payment1 = new Payment(null,Instant.parse("2019-06-20T19:53:07Z"),o1);
+        o1.setPayment(payment1);
+
+        orderRepository.saveAll(Arrays.asList(o1));
+
     }
 }
