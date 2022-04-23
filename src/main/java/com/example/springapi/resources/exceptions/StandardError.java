d767a4f2bde6af85462965816ee7f572578b35ac
DESKTOP-R8GAO0T\JhonyPC <jhonymarlon@gmail.com>
2022-04-23 19:40:23 -0300

    Adding exception handling to get findById

--- src/main/java/com/example/springapi/resources/exceptions/StandardError.java
+++ src/main/java/com/example/springapi/resources/exceptions/StandardError.java
@@ -1,9 +1,12 @@
 package com.example.springapi.resources.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.io.Serializable;
 import java.time.Instant;
 
 public class StandardError implements Serializable {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant timestamp;
     private Integer status;
     private String error;
