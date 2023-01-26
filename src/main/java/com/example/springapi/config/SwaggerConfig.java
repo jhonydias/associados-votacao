19f5fea8daeaca9417acfc2de1a0d8f999636ba2
jhony.dias <jhony.dias@techlead.com.br>
2023-01-25 23:11:00 -0300

    Adding Swagger 2 Configuration and the edit the value of the context-path to /ecommerce-project

--- src/main/java/com/example/springapi/config/SwaggerConfig.java
+++ src/main/java/com/example/springapi/config/SwaggerConfig.java
@@ -1,47 +1,75 @@
 package com.example.springapi.config;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.VendorExtension;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.service.Contact;
 
 import java.util.ArrayList;
-
-import static springfox.documentation.builders.PathSelectors.regex;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    public static final String
+            USUARIO_TAG = "Usuário",
+            PERFIL_TAG = "Perfil",
+            VALIDACAO_TAG = "Validação";
 
     @Bean
-    public Docket productApi() {
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
+
+    @Bean
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(metaData())
+                //.securityContexts(Collections.singletonList(securityContext()))
+                //.securitySchemes(Collections.singletonList(apiKey()))
+                .useDefaultResponseMessages(false)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.springapi"))
-                .paths(regex("/api.*"))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
                 .build()
-                .apiInfo(metaInfo());
+                .tags(new Tag(USUARIO_TAG, "Realizar operações com usuários"),
+                        new Tag(PERFIL_TAG, "Realizar operações com perfis"),
+                        new Tag(VALIDACAO_TAG, "Realizar operações de validação"));
     }
 
-    private ApiInfo metaInfo() {
 
-        ApiInfo apiInfo = new ApiInfo(
-                "Produtos API REST",
-                "API REST de cadastro de produtos.",
-                "1.0",
-                "Terms of Service",
-                new Contact("Jhony Dias", "https://github.com/jhonydias/spring-users-api",
-                        "jhonydias.work@gmail.com"),
-                "Apache License Version 2.0",
-                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
-        );
-
-        return apiInfo;
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Produtos API REST")
+                .description("API REST de cadastro de produtos.")
+                .version("1.0.0")
+                .contact(new Contact("Jhony Dias",
+                        "https://github.com/jhonydias/spring-users-api",
+                        "jhonydias.work@gmail.com"))
+                .build();
     }
 
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
+    }
 }
--- src/main/java/com/example/springapi/resources/UserResource.java
+++ src/main/java/com/example/springapi/resources/UserResource.java
@@ -1,7 +1,10 @@
 package com.example.springapi.resources;
 
+import com.example.springapi.config.SwaggerConfig;
 import com.example.springapi.entities.User;
 import com.example.springapi.services.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,24 +15,29 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping("api/usuarios")
+@Api(value = "Usuários", tags = {SwaggerConfig.USUARIO_TAG})
+
 public class UserResource {
 
     @Autowired
     private UserService service;
 
+    @ApiOperation(value = "Lista usuários do sistema")
     @GetMapping("/users")
     public ResponseEntity<List<User>> findAll() {
         List<User> list = service.findAll();
                 return ResponseEntity.ok().body(list);
     }
 
+    @ApiOperation(value = "Lista usuários do sistema por id")
     @GetMapping(value = "/users/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    @ApiOperation(value = "Adiciona um novo usuário ao sistema")
     @PostMapping("/users")
     public ResponseEntity<User> insert(@RequestBody User obj){
         obj = service.insert(obj);
@@ -37,12 +45,14 @@ public class UserResource {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    @ApiOperation(value = "Exclui o usuário de id pesquisado da base de dados")
     @DeleteMapping(value = "/users/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id){
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
+    @ApiOperation(value = "Altera as informações do usuário do id pesquisado cadastrado no sistema")
     @PutMapping(value = "/users/{id}")
     public ResponseEntity<User> update(@PathVariable Long id,@RequestBody User obj ){
         obj = service.update(id,obj);
